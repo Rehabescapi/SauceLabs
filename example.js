8c10ea48fe7c352f47143d6366044effd6d4e3ef
Jason Lehmann <jason.lehmann@aspiritech.org>
2021-06-10 21:45:56 -0500

    Adding Cypress

--- example.js
+++ example.js
@@ -9,12 +9,11 @@ driver.get("http://www.google.com");
 driver.findElement(By.name("q")).sendKeys("webdriver");
 
 driver.sleep(1000).then(function () {
-  driver.findElement(By.name("q")).sendKeys(webdriver.Key.TAB);
+  driver.findElement(By.name("q")).sendKeys(webdriver.Key.ENTER);
 });
 
-driver.findElement(By.name("btnK")).click();
-
 driver.sleep(2000).then(function () {
+  driver.findElement(By.name("btnK")).click();
   driver.getTitle().then(function (title) {
     if (title === "webdriver - Google Search") {
       console.log("Test passed");
